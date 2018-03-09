44d172b98bea17fb88dbab4b645fad55accf31a5
刘宾 <915249126@qq.com>
2018-03-09 18:19:37 +0800

    modify UI

---
+++ articles/all.js
@@ -0,0 +1,16 @@
+var allArticles = [
+    {
+        title:'SVG相关学习(一)SVG基础',
+        tags:['SVG','动画'],
+        target:'https://github.com/liubin915249126/HTML-CSS-SVG/tree/master/SVG#svg-%E7%9B%B8%E5%85%B3%E5%AD%A6%E4%B9%A0',
+        img:{src:'https://github.com/liubin915249126/HTML-CSS-SVG/raw/master/SVG/image/SVGArcs_Flags.png',alt:'SVGArcs_Flags'},
+        description:'主要包括SVG基础知识，基本图形，渐变，还有path路径相关'
+    },
+    {
+        title:'连接两个div的直线，拖动时保持连线',
+        tags:['SVG','动画'],
+        target:'https://github.com/liubin915249126/javascript/tree/master/SVG#%E7%94%A8svg%E8%BF%9E%E6%8E%A5%E4%B8%A4%E4%B8%AAdiv%E7%9A%84%E7%9B%B4%E7%BA%BF%E6%8B%96%E5%8A%A8%E5%AE%9E%E6%97%B6%E6%94%B9%E5%8F%98%E4%BD%8D%E7%BD%AE',
+        img:{src:'https://github.com/liubin915249126/javascript/raw/master/SVG/image/svgDrag.gif',alt:'效果图'},
+        description:'连接两个div带箭头的虚线，拖动div时保持线条实时改变位置保持始终连接'
+    }
+]

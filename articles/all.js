b5524ec18b11a8f76e34e2b7c05d2002983bc847
刘宾 <915249126@qq.com>
2018-03-10 17:56:58 +0800

    add new article

--- articles/all.js
+++ articles/all.js
@@ -9,7 +9,7 @@ var allArticles = [
     {
         title:'连接两个div的直线，拖动时保持连线',
         tags:['SVG','动画'],
-        target:'https://github.com/liubin915249126/javascript/tree/master/SVG#%E7%94%A8svg%E8%BF%9E%E6%8E%A5%E4%B8%A4%E4%B8%AAdiv%E7%9A%84%E7%9B%B4%E7%BA%BF%E6%8B%96%E5%8A%A8%E5%AE%9E%E6%97%B6%E6%94%B9%E5%8F%98%E4%BD%8D%E7%BD%AE',
+        target:'svg3.html',
         img:{src:'https://github.com/liubin915249126/javascript/raw/master/SVG/image/svgDrag.gif',alt:'效果图'},
         description:'连接两个div带箭头的虚线，拖动div时保持线条实时改变位置保持始终连接'
     },

45f10a92ab33e3e96a25a3f629ccd2be57fe001f
刘宾 <915249126@qq.com>
2018-03-13 10:35:57 +0800

    modify ui

--- articles/all.js
+++ articles/all.js
@@ -4,42 +4,48 @@ var allArticles = [
         tags:['SVG','动画'],
         target:'svg2.html',
         img:{src:'https://github.com/liubin915249126/HTML-CSS-SVG/raw/master/SVG/image/SVGArcs_Flags.png',alt:'SVGArcs_Flags'},
-        description:'主要包括SVG基础知识，基本图形，渐变，还有path路径相关'
+        description:'主要包括SVG基础知识，基本图形，渐变，还有path路径相关',
+        creattime:'2018-03-08'
     },
     {
         title:'连接两个div的直线，拖动时保持连线',
         tags:['SVG','动画'],
         target:'svg3.html',
         img:{src:'https://github.com/liubin915249126/javascript/raw/master/SVG/image/svgDrag.gif',alt:'效果图'},
-        description:'连接两个div带箭头的虚线，拖动div时保持线条实时改变位置保持始终连接'
+        description:'连接两个div带箭头的虚线，拖动div时保持线条实时改变位置保持始终连接',
+        creattime:'2018-03-08'
     },
     {
         title:'原生js实现视差滚动效果（类似D3官网的效果），兼容IE',
         tags:['javascript','动画','Parallax'],
         target:'parallax.html',
         img:{src:'https://github.com/liubin915249126/javascript/raw/master/Parallax/img/parallax.gif',alt:'效果图'},
-        description:'鼠标在图片上上下左右滑动时,让图片整体转动,转动方向与鼠标移动的方向相同,从而实现视差效果'
+        description:'鼠标在图片上上下左右滑动时,让图片整体转动,转动方向与鼠标移动的方向相同,从而实现视差效果',
+        creattime:'2018-03-08'
     },
     {
         title:'jquery组织架构图插件',
         tags:['javascript','动画','jquery','jquery插件','orgnization'],
         target:'lenchart.html',
         img:{src:'https://github.com/liubin915249126/javascript/raw/master/lencharts/image/lenchart.gif',alt:'效果图'},
-        description:'使用递归的方式深层遍历数据,架构图第二层为横向结构,从第三层开始为竖向结构,解决了横向太长的问题。点击加减号按钮可以显示与隐藏当前节点的子节点。'
+        description:'使用递归的方式深层遍历数据,架构图第二层为横向结构,从第三层开始为竖向结构,解决了横向太长的问题。点击加减号按钮可以显示与隐藏当前节点的子节点。',
+        creattime:'2018-03-08'
     },
     {
         title:'js判断鼠标划入方向',
         tags:['javascript','动画','jquery','mouse'],
         target:'../example/mousedirction.html',
         img:{src:'https://github.com/liubin915249126/javascript/raw/master/image/dirction.gif',alt:'效果图'},
-        description:'根据鼠标滑入方向定制相应动画'
+        description:'根据鼠标滑入方向定制相应动画',
+        creattime:'2018-03-10'
     },
     {
         title:'react里面使用jquery插件',
         tags:['javascript','jquery','react','jquery插件'],
-        target:'https://github.com/liubin915249126/react-study/blob/master/jquery-in-react.md#%E5%9C%A8react%E9%87%8C%E9%9D%A2%E4%BD%BF%E7%94%A8jquery%E6%8F%92%E4%BB%B6',
+        target:'../example/jquery-in-react.html',
         // img:{src:'https://github.com/liubin915249126/javascript/raw/master/image/dirction.gif',alt:'效果图'},
-        description:'webpack支持CMD，AMD语法，CMD，AMD的语法写文件，然后引入，在组件componentDidMount里面调用方法'
+        description:'webpack支持CMD，AMD语法，CMD，AMD的语法写文件，然后引入，在组件componentDidMount里面调用方法',
+        creattime:'2018-03-12'
     }
     
 ]
